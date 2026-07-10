bcc9e7739b67797a83a53b2baa0a71664551e5a8
Aditya-hk <aditya.upadhyaya11@gmail.com>
2026-07-10 22:53:50 +0530

    Change WhisperModel to use 'small' variant

--- src/transcriber.py
+++ src/transcriber.py
@@ -1,13 +1,16 @@
 from faster_whisper import WhisperModel
 
-model = WhisperModel("large-v3", device="cpu",          # Change to "cuda" if using NVIDIA GPU
-    compute_type="int8")
-
-
+model = WhisperModel(
+    "small",
+    device="cpu",
+    compute_type="int8"
+)
 
 def transcribe(audio_path):
     segments, info = model.transcribe(audio_path)
-    transcript=[]
+
+    transcript = []
+
     for segment in segments:
         transcript.append({
             "start": round(segment.start, 2),
